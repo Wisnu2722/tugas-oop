6bbec23ae55723c95096506882c8accbfa4ab75b
Ryan Gonz <wisnu2722@students.amikom.ac.id>
2023-12-12 03:42:32 +0700

    commit product class

---
+++ product.js
@@ -0,0 +1,16 @@
+export default class Product {
+    constructor(productId, productName, price, description) {
+        this.productId = productId
+        this.productName = productName
+        this.price = price
+        this.description = description
+    }
+    
+    displayInfo() {
+
+    }
+    
+    updateProduct() {
+    }
+
+}
