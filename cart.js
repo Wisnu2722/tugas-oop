8ad300e1dc3bb686cbd0d5e3839a7145e9823616
Ryan Gonz <wisnu2722@students.amikom.ac.id>
2023-12-13 23:51:56 +0700

    added removeItem method

--- cart.js
+++ cart.js
@@ -1,4 +1,5 @@
 import formatCurrency from "./helper.js"
+import Table  from "cli-table3"
 
 export default class Cart {
 
@@ -14,25 +15,50 @@ export default class Cart {
 
     removeItem(product) {
 
+        const removedIndex = this.items.findIndex(item => item.product === product);
+
+        if (removedIndex !== -1) {
+            this.items.splice(removedIndex, 1);
+        }
+
+        console.log('\n')
+        console.log('Item successfuly deleted')
+        this.viewCart()
+        
     }
 
     viewCart() {
 
-        console.log(`cart id: ${this.cartId}`)
+        
+        console.log(`Cart ID: ${this.cartId}`)
         const allItems = this.items
         let subTotal
         let total = 0
 
+        const productInCart = new Table({
+            head: ['Product Name', 'Price', 'Qty', 'Sub Total'],
+            style: { head: [], border: [] }
+        });
+
         allItems.forEach(element => {
-            console.log(`name: ${element['product']['productName']}`)
-            console.log(`price: ${formatCurrency(element['product']['price'])}`)
-            console.log(`qty: ${element.qty}`)
+            let product_name = `${element['product']['productName']}`
             subTotal = element['product']['price'] * element.qty
-            console.log(`sub total: ${formatCurrency(subTotal)}`)
+            productInCart.push([
+                { content: product_name },
+                { content: `${formatCurrency(element['product']['price'])}` },
+                { content: element.qty },
+                { content: subTotal },
+            ])
+
+            // console.log(`price: ${formatCurrency(element['product']['price'])}`)
+            // console.log(`qty: ${element.qty}`)
+            // console.log(`sub total: ${formatCurrency(subTotal)}`)
             total += subTotal
         });
 
-        console.log(formatCurrency(total))
+        console.log(productInCart.toString())
+        console.log(`Total: ${formatCurrency(total)}`)
+        console.log('\n')
 
     }
 }
