dad874bcdfc9dc7a57f3176fa68c6f3b49c36568
Ryan Gonz <wisnu2722@students.amikom.ac.id>
2023-12-13 23:07:41 +0700

    add method addItem and viewCart

--- cart.js
+++ cart.js
@@ -1,12 +1,15 @@
+import formatCurrency from "./helper.js"
+
 export default class Cart {
 
-    constructor(cartId ) {
+    constructor(cartId) {
         this.cartId = cartId
         this.items = []
+        this.qty = []
     }
 
-    addItemm(product, qty) {
-
+    addItem(product, qty) {
+        this.items.push({ product, qty })
     }
 
     removeItem(product) {
@@ -14,6 +17,22 @@ export default class Cart {
     }
 
     viewCart() {
+
+        console.log(`cart id: ${this.cartId}`)
+        const allItems = this.items
+        let subTotal
+        let total = 0
+
+        allItems.forEach(element => {
+            console.log(`name: ${element['product']['productName']}`)
+            console.log(`price: ${formatCurrency(element['product']['price'])}`)
+            console.log(`qty: ${element.qty}`)
+            subTotal = element['product']['price'] * element.qty
+            console.log(`sub total: ${formatCurrency(subTotal)}`)
+            total += subTotal
+        });
+
+        console.log(formatCurrency(total))
 
     }
 }
