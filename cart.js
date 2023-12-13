7176509428138f81d632aa283fc03fcf948d6c2c
Ryan Gonz <wisnu2722@students.amikom.ac.id>
2023-12-13 21:49:20 +0700

    initiate class cart

---
+++ cart.js
@@ -0,0 +1,19 @@
+export default class Cart {
+
+    constructor(cartId ) {
+        this.cartId = cartId
+        this.items = []
+    }
+
+    addItemm(product, qty) {
+
+    }
+
+    removeItem(product) {
+
+    }
+
+    viewCart() {
+
+    }
+}
