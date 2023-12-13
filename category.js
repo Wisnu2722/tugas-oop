804725c022dac36835747ec7d2693ba3b32fbc7a
Ryan Gonz <wisnu2722@students.amikom.ac.id>
2023-12-13 21:45:07 +0700

    add method displayInfo, addProduct, updateCategory, and productList

--- category.js
+++ category.js
@@ -1,21 +1,46 @@
-class Category {
-    products = []
+import Table from "cli-table3"
+import formatCurrency from "./helper.js"
+
+export class Category {
 
     constructor(CategoryId, categoryName) {
         this.CategoryId = CategoryId
         this.categoryName = categoryName
+        this.products = []
     }
 
     displayInfo() {
-
+        console.log(`Category ID: ${this.CategoryId}`)
+        console.log(`Category Name: ${this.categoryName}`)
     }
 
-    updateCategory(){
-
+    addProduct(dataProduct) {
+        this.products.push(dataProduct)
     }
 
-    products(){
-        
+    updateCategory(newCategoryName) {
+        this.categoryName = newCategoryName
+        console.log(`Category name has been updated to: ${this.categoryName}`)
     }
 
+    productList() {
+        const productArray = this.products
+        const productTable = new Table({
+            head: ['Product Name', 'Price', 'Desc'],
+            style: { head: [], border: [] }
+        });
+
+        console.log('\n')
+        console.log(`Products in ${this.categoryName} category `)
+
+        productArray.forEach(element => {
+            productTable.push([
+                { content: element.productName },
+                { content: formatCurrency(element.price) },
+                { content: element.description },
+            ])
+        });
+
+        console.log(productTable.toString())
+    }
 }
